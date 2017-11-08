09d7f0b3f8857ce8a3d0340b60af6770104c552e
Weezer Su <weezer@users.noreply.github.com>
2017-11-08 16:59:41 -0600

    Update start.sh
    fix sleep

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo "Hello Kubernetes" > /usr/share/nginx/html/index.html
 nginx &
-sleep tr -cd 0-9 </dev/urandom | head -c 2
+sleep `tr -cd 0-9 </dev/urandom | head -c 2`
 stress --vm 4 --timeout 100 --vm-bytes 6000M
